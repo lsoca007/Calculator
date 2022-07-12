a091301bacd9cb364b68fdda7706afae63b6eca2
lsoca007 <77649258+lsoca007@users.noreply.github.com>
2022-07-12 16:33:31 -0400

    Create index.html

--- calculator.js
+++ calculator.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req,res){ res.send("Hello world!");
+app.get("/", function(req,res){
+                              res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000,
